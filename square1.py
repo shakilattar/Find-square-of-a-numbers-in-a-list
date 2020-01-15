6e4bf83245a7af45043470efcfe233fba9edd8b5
shakilattar <59828272+shakilattar@users.noreply.github.com>
2020-01-15 17:55:17 +0530

    Create square1.py

---
+++ square1.py
@@ -0,0 +1,12 @@
+# Find square of numbers in the list
+def fun1():
+    # create a list
+    numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    print(numbers)
+    for number in numbers:
+        square = number * number
+        print(number)
+        print(square)
+
+
+fun1()
